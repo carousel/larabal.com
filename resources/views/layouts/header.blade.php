9a887e776a0f6bc6e432e145d8e91b8e016e8f00
miroslav <miroslav.trninic@gmail.com>
2015-04-14 00:19:50 +0200

    styling prettyprint

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,7 +3,7 @@
 <div class="container">
         <div class="row">
             <div class="col-lg-8 col-md-12 col-sm-6 head page-header" id="banner">
-                <h1>larabal.com (test)</h1>
+                <h1>larabal.com </h1>
                 <p class="leader">Welcome to Laravel Balkan, your source of information about <a href="http://laravel.com">laravel</a> framework in this part of the world.</p>
             </div>
     </div>
